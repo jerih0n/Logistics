cd76283c7fd0f7a9ac6ef8036091d5b317dcc437
Miro1187 <52543759+Miro1187@users.noreply.github.com>
2020-11-23 15:02:57 +0200

    new version

--- Logistics.Web/Controllers/HomeController.cs
+++ Logistics.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Logistics.Common.Models;
 using Logistics.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,9 +7,26 @@ namespace Logistics.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDBAccess _dbAccess;
+
+        public HomeController(IDBAccess dbAccess)
+        {
+            _dbAccess = dbAccess;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        //[HttpPost]
+        public void Update()
+        {
+            if (ModelState.IsValid)
+            {
+                _dbAccess.UpdateLogistics();
+                _dbAccess.SaveChanges();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
